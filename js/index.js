69c485f46b18f7f8d3bda6fdfbb0551f4f52d723
Isensee <17isenseed@kisdtx.com>
2017-01-13 14:57:46 -0600

    Added another div

--- js/index.js
+++ js/index.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
   $(".name").hide();
   $(".name").fadeIn(4000);
 
+  $(window).scroll(function() {
+    var nameTop = $("#name").height();
+    if ($(this).scrollTop() >= nameTop) {
+      $("#name").slideUp();
+    }
+    else if ($(this).scrollTop() < nameTop) {
+      $("#name").show("slow");
+    }
+  });
 });
